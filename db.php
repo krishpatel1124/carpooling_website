f1b4a183a6baca7ded3d9be379e9029b221c2734
krishpatel1124 <krishpatel1214@gmail.com>
2025-06-09 18:26:57 +0530

    password

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 // Database connection settings for db4free.net
 $servername = "db4free.net"; // Host of the remote database
-$username = "your_db_username"; // Replace with your db4free username
-$password = "your_db_password"; // Replace with your db4free password
+$username = "root"; // Replace with your db4free username
+$password = ""; // Replace with your db4free password
 $dbname = "ride"; // Database name you created on db4free
 
 // Create connection
